64fb3608bdf265453e8f9bb6f76eb01d7b7d7313
Luiz E <doginuwu@gmail.com>
2024-05-06 21:31:04 -0300

    feat: add gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
+import partytown from "@astrojs/partytown";
 
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [
+    vue(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });
