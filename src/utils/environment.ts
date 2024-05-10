a3d50f11a46c03cdac3f4d8e02350e7d0c22731c
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2024-05-10 09:26:52 -0300

    add competitions url and some minor changes

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,15 +1,17 @@
 function getFromEnv(key: string, defaultValue?: string): string {
-    const value = import.meta.env[key];
+  const value = import.meta.env[key];
 
-    if(!value && !defaultValue) throw new Error(`Environment variable ${key} is not set`);
+  if (!value && !defaultValue)
+    throw new Error(`Environment variable ${key} is not set`);
 
-    return value || defaultValue;
+  return value || defaultValue;
 }
 
 export const config = {
-    hemocioneEventsApi: getFromEnv('HEMOCIONE_EVENTS_API'),
-    hemocioneEvents: getFromEnv('HEMOCIONE_EVENTS'),
-    hemocioneApp: getFromEnv('HEMOCIONE_APP'),
-    hemocioneDonation: getFromEnv('HEMOCIONE_DONATION'),
-    hemocioneStore: getFromEnv('HEMOCIONE_STORE'),
+  hemocioneEventsApi: getFromEnv("HEMOCIONE_EVENTS_API"),
+  hemocioneEvents: getFromEnv("HEMOCIONE_EVENTS"),
+  hemocioneApp: getFromEnv("HEMOCIONE_APP"),
+  hemocioneDonation: getFromEnv("HEMOCIONE_DONATION"),
+  hemocioneStore: getFromEnv("HEMOCIONE_STORE"),
+  hemocioneCompetitions: getFromEnv("HEMOCIONE_COMPETITIONS"),
 } as const;
